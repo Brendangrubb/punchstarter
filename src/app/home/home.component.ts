9abe82ad673bd926a45fce6b92e4c552bab6aaf8
Ash Laidlaw and Brendan Grubb <ash@>
2017-04-05 10:30:34 -0700

    Add dependency injection

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,16 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { Invention } from '../invention.model';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
+import { InventionService } from '../invention.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  providers: [InventionService]
 })
 export class HomeComponent implements OnInit {
+  inventions: FirebaseListObservable<any[]>;
 
-  constructor() { }
+  constructor(private inventionService: InventionService, private router: Router) { }
 
   ngOnInit() {
+    this.inventions = this.inventionService.getInventions();
   }
 
 }
---
+++ src/app/invention.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { Invention } from './invention.model';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
+
+@Injectable()
+export class InventionService {
+  inventions: FirebaseListObservable<any[]>;
+
+  constructor(private angularFire: AngularFire) {
+    this.inventions = angularFire.database.list('inventions');
+  }
+
+  getInventions() {
+    return this.inventions;
+  }
+
+}
